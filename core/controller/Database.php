85fa971e44b2b7b18e8858bccd40cc06b135d2be
Bárbara <barbara.lopez@ikasle.egibide.org>
2022-02-12 15:59:26 +0100

    bbdd

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,10 +3,11 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+		$this->user="bc3391548c80be";$this->pass="aaa94689";$this->host="us-cdbr-east-05.cleardb.net";$this->ddbb="heroku_12f3139c169dc03";
 	}
 
 	function connect(){
+		$this->user="bc3391548c80be";$this->pass="aaa94689";$this->host="us-cdbr-east-05.cleardb.net";$this->ddbb="heroku_12f3139c169dc03";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
